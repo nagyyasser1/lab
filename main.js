d8e74241c0b2eca7f226764ab050d1fadc04090a
nagy <nagyy8751@gmail.com>
2023-05-04 19:44:22 +0200

    v2

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ openMenuIcon.onclick = () => {
   if (openMenuIcon.className === 'fa-solid fa-bars') {
     //open
     openMenuIcon.className = 'fa-solid fa-xmark'
+
     sideNav.style.right = '0px'
   } else {
     openMenuIcon.className = 'fa-solid fa-bars' //close
+
     sideNav.style.right = '-400px'
   }
 }
